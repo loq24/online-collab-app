c87ff4e06e3607eeee88d1b377c07f0a16ea892a
Lougie <lougie.quisel@gmail.com>
2024-08-15 22:46:02 +0800

    Add Header component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,14 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 export default function Document() {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title.</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   );
