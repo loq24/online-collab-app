4e501f63c4b1cf269e2162ce5a06fc1036796d08
loq24 <lougie.quisel@gmail.com>
2025-02-20 23:09:28 +0800

    feat: send notification

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -98,7 +98,21 @@ export const updateDocumentAccess = async ({
     const room = await liveblocks.updateRoom(roomId, { usersAccesses });
 
     if (room) {
-      //TODO : send a notification to the user
+      const notificationId = nanoid();
+
+      await liveblocks.triggerInboxNotification({
+        userId: email,
+        kind: "$documentAccess",
+        subjectId: notificationId,
+        activityData: {
+          userType,
+          title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+          updatedBy: updatedBy.name,
+          avatar: updatedBy.avatar,
+          email: updatedBy.email,
+        },
+        roomId,
+      });
     }
 
     revalidatePath(`/documents/${roomId}`);
