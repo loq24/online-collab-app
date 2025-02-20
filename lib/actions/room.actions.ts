77c551e40d1bc71dfa337737facc016999497baa
loq24 <lougie.quisel@gmail.com>
2025-02-20 23:01:53 +0800

    feat: implement modals

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -4,6 +4,7 @@ import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { getAccessType, parseStringify } from "../utils";
+import { redirect } from "next/navigation";
 
 export const createDocument = async ({
   userId,
@@ -131,5 +132,15 @@ export const removeCollaborator = async ({
     return parseStringify(updatedRoom);
   } catch (error) {
     console.log(`Error happened while removing a collaborator: ${error}`);
+  }
+};
+
+export const deleteDocument = async (roomId: string) => {
+  try {
+    await liveblocks.deleteRoom(roomId);
+    revalidatePath(`/`);
+    redirect(`/`);
+  } catch (error) {
+    console.log(`Error happened while removing a document: ${error}`);
   }
 };
